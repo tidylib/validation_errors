2ad290d42e8950e8dcab2f6cabd6c5164b8581f4
Suman Gurung <sumangurung@gmail.com>
2015-03-15 16:56:18 -0400

    Minimal implementation of #empty?

--- lib/tidylib/validation_errors.rb
+++ lib/tidylib/validation_errors.rb
@@ -1,5 +1,18 @@
 module Tidylib
   class ValidationErrors
     VERSION = "0.1.0"
+
+    def initialize
+      @empty = true
+
+    end
+
+    def empty?
+      @empty
+    end
+
+    def add(topic, message)
+      @empty = false
+    end
   end
 end
--- spec/tidylib/validation_errors_spec.rb
+++ spec/tidylib/validation_errors_spec.rb
@@ -1,7 +1,20 @@
 require 'spec_helper'
 
-describe Tidylib::ValidationErrors do
+RSpec.describe Tidylib::ValidationErrors do
   it 'has a version number' do
     expect(Tidylib::ValidationErrors::VERSION).not_to be nil
+  end
+
+  describe "#empty?" do
+    it "returns true with no errors" do
+      errors = described_class.new
+      expect(errors.empty?).to be_truthy
+    end
+
+    it "returns false with errors" do
+      errors = described_class.new
+      errors.add(:name, :present)
+      expect(errors.empty?).to be_falsey
+    end
   end
 end
